0a1b9f7672add7ddcb5c28ba6c57e21c2ab403b9
Justin Ma <justin.ma8419@rgp.com>
2020-08-25 12:44:49 -0400

    "chp8 is added"

---
+++ exc_chp8.py
@@ -0,0 +1,16 @@
+# 8.1
+# def display_message():
+#     print('I am learning about function in this chapter.')
+#
+#
+# display_message()
+
+
+
+# 8.2
+
+def favorite_book(book_name):
+    print(f"My favorite book is {book_name}")
+
+
+favorite_book('Alice in Wonderland')
