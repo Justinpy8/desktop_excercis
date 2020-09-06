b3cd6b9903cd1e3bbcdbca59f3e6f5c621c51ca5
Justin Ma <justin.ma8419@gmail.com>
2020-09-06 16:29:45 -0400

    [Justin Ma] Google Selenium test exercise is done.

--- test_google_search.py
+++ test_google_search.py
@@ -17,14 +17,35 @@ time.sleep(2)
 search_box.send_keys(Keys.RETURN)
 time.sleep(1)
 
-result_msg_list = list()
 result_msg = driver.find_element_by_xpath("//div[@id='result-stats']").text
 print(result_msg)
-
-result_msg_list.append(result_msg)
-
-print('*****result are being saved into a list*****')
+result_msg_list = result_msg.split()
 print(result_msg_list)
 
-result_num.split() = result_msg_list
-print(result_num)
+print("Refining the number of results:")
+result_msg_num = int(result_msg_list[1].replace(",", ""))
+# result_numbers = int(result_msg_num.replace(",", ""))
+print(result_msg_num)
+
+print("Refining the result time:")
+result_msg_time = float(result_msg_list[3].replace("(", ""))
+# result_time = float(result_msg_time.replace("(", ""))
+print(result_msg_time)
+
+assert result_msg_num > 20000000, "Results are less than 20 millions."
+print("Verifying result number is passed.")
+
+assert result_msg_time < 1, "The search took more than 1 second."
+print("Verifying search performance passed.")
+
+# if result_msg_num > 2000000:
+#     print("It returned more than 2 million results")
+# else:
+#     print("It returned less than 2 million results")
+#
+# if result_msg_time < 2:
+#     print("It took less than 2 seconds to complete the search.")
+# else:
+#     print("It took more than 2 seconds to complete the search.")
+print("Test is completed, closing the browser...........")
+driver.close()
