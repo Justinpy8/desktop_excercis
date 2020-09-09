06bfe6beeb2f9c7853f27fcc41e4581843d051fa
Justin Ma <justin.ma8419@gmail.com>
2020-09-09 00:36:06 -0400

    [Justin Ma] Yahoo Mail login and filter added.

--- yahoo_login.py
+++ yahoo_login.py
@@ -12,15 +12,29 @@ username_input.clear()
 username_input.send_keys("sleepingbeaz@yahoo.com")
 username_input.send_keys(Keys.RETURN)
 time.sleep(1)
-pw = driver.find_element_by_id("login-passwd")
-pw.send_keys("Zhixiang84")
-driver.find_element_by_xpath('//*[@id="login-signin"]').click()
+password_input = driver.find_element_by_id("login-passwd")
+password_input.send_keys("Zhixiang84")
+driver.find_element_by_xpath('//*[@id="login-signin"]').send_keys(Keys.RETURN)
+time.sleep(2)
+driver.find_element_by_link_text("Mail").click()
+time.sleep(1)
+driver.find_element_by_xpath(
+    '//*[@id="mail-app-component"]/div[1]/div/div[2]/div/div/div[3]/div/div[1]/ul/li[3]/a').click()
+time.sleep(2)
+incoming_email_address = driver.find_element_by_xpath("//span[@class='u_N C_Z1VRpVF']").text
+print(incoming_email_address)
+email_address = incoming_email_address.split('@')
+email = email_address[1].replace(">", "")
+print(f"Email domain: {email}")
 
+if email == "amazon.com":
+    print("Moving to the next email.")
+    driver.find_element_by_xpath("//div[@class='ab_C k_w D_F H_7bcz en_0 P_2bJhi p_R m_Z14vXdP I_52qC gl_FM']//span[2]//button[1]//span[1]").click()
+else:
+    print("Closing the email.")
+    driver.find_element_by_link_text("Delete").click()
 
-# time.sleep(2)
-# pw_input.send_keys(Keys.RETURN)
-
-
+# driver.find_element_by_xpath("//span[contains(text(),'Delete')]").click()
 
 
 # GMAIL Login:
@@ -35,7 +49,3 @@ driver.find_element_by_xpath('//*[@id="login-signin"]').click()
 # password_input = driver.find_element_by_name('password')
 # password_input.send_keys('20190103')
 # driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button').click()
-
-
-
-
